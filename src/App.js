7ebecb54e1bec0317f16079f5b876229dd7b811c
wittori <wittoria@ukr.net>
2023-04-04 11:43:50 +0300

    Make functions(response,event)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello</h1>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./Weather.css";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      feels_like: response.data.main.feels_like,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        className="form-control"
+        onChange={updateCity}
+      />
+
+      <button type="submit" className="search-button">
+        Search
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="Weather">
+      {form}
+      <div className="row">
+        <div className="col-6">
+          <h1>{city}</h1>
+          <h2>
+            <span>{weather.date}</span>
+          </h2>
+          <br />
+          <h3>{weather.description}</h3>
+          <br />
+          <button type="submit" className="currentLocationButton">
+            Current location
+          </button>
+        </div>
+        <div className="col-6">
+          <div className="col-6 weatherNow">
+            <ul>
+              <li classNames="tempNow">
+                <strong>{Math.round(weather.temperature)}</strong>
+                <a href="/" className="celsiusTemp">
+                  °C
+                </a>
+                |
+                <a href="/" className="farenheitTemp">
+                  °F
+                </a>
+              </li>
+              <li className="feelsLike">
+                Feels like: {Math.round(weather.feels_like)} °C
+              </li>
+              <li className="humidity">Humidity: {weather.humidity} %</li>
+              <li className="wind">Wind: {weather.wind} km/h</li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <img
+              src={weather.icon}
+              alt={weather.description}
+              className="mainWeatherPic"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
