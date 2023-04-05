3de7e80911ef8256e8acbf159c0bee245e353f1f
wittori <wittoria@ukr.net>
2023-04-05 14:06:02 +0300

    Add bootstrap, css in app.css, footer in app, change css in weather.css and add if statement

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          Coded by Viktoria Fedash {""}
+          <a
+            href="https://github.com/wittori/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          {""} and {""}
+          <a
+            href="https://candid-beignet-efde6d.netlify.app/"
+            target="-blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,12 +3,14 @@ import axios from "axios";
 
 import "./Weather.css";
 
-export default function Weather() {
-  const [city, setCity] = useState("");
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
 
   function displayWeather(response) {
     console.log(response.data);
+    setLoaded(true);
     setWeather({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
@@ -29,67 +31,97 @@ export default function Weather() {
     event.preventDefault();
     setCity(event.target.value);
   }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        className="form-control"
-        onChange={updateCity}
-      />
-
-      <button type="submit" className="search-button">
-        Search
-      </button>
-    </form>
-  );
-
-  return (
-    <div className="Weather">
-      {form}
-      <div className="row">
-        <div className="col-6">
-          <h1>{city}</h1>
-          <h2>
-            <span>{weather.date}</span>
-          </h2>
-          <br />
-          <h3>{weather.description}</h3>
-          <br />
-          <button type="submit" className="currentLocationButton">
-            Current location
-          </button>
-        </div>
-        <div className="col-6">
-          <div className="col-6 weatherNow">
-            <ul>
-              <li classNames="tempNow">
-                <strong>{Math.round(weather.temperature)}</strong>
-                <a href="/" className="celsiusTemp">
-                  °C
-                </a>
-                |
-                <a href="/" className="farenheitTemp">
-                  °F
-                </a>
-              </li>
-              <li className="feelsLike">
-                Feels like: {Math.round(weather.feels_like)} °C
-              </li>
-              <li className="humidity">Humidity: {weather.humidity} %</li>
-              <li className="wind">Wind: {weather.wind} km/h</li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <img
-              src={weather.icon}
-              alt={weather.description}
-              className="mainWeatherPic"
-            />
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <h1>{city}</h1>
+              <h2>
+                <span>{weather.date}</span>
+              </h2>
+              <br />
+              <h3>{weather.description}</h3>
+              <br />
+              <button type="submit" className="currentLocationButton">
+                Current location
+              </button>
+            </div>
+            <div className="col-6">
+              <form onSubmit={handleSubmit}>
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city"
+                    className="form-control"
+                    autoFocus="on"
+                    onChange={updateCity}
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </form>
+              <div className="col-6 weatherNow">
+                <ul>
+                  <li classNames="tempNow">
+                    <strong>{Math.round(weather.temperature)}</strong>
+                    <a href="/" className="celsiusTemp">
+                      °C
+                    </a>
+                    |
+                    <a href="/" className="farenheitTemp">
+                      °F
+                    </a>
+                  </li>
+                  <li className="feelsLike">
+                    Feels like: {Math.round(weather.feels_like)} °C
+                  </li>
+                  <li className="humidity">Humidity: {weather.humidity} %</li>
+                  <li className="wind">Wind: {weather.wind} km/h</li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <img
+                  src={weather.icon}
+                  alt={weather.description}
+                  className="mainWeatherPic"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
